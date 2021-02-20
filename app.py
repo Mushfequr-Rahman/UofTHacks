6d2546314a010d350241c91b830894367173670b
Alireza Hajebrahimi <mrex778@yahoo.ca>
2021-02-21 07:58:33 +0900

    [Add] Tailwindcss

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+'''
+Filename: app.py
+- Main file for displaying
+the project
+'''
+
+# MARK: - Libraries
 from flask import Flask, request, render_template
+
+
 app = Flask(__name__)
 
+# MARK: - / Route
 @app.route('/')
 def index(): 
     return render_template('index.html')
 
+# MARK: - /renderLogin Route
 @app.route('/renderLogin')
 def renderLogin(): 
     return render_template('login.html') 
 
+# MARK: - /login Route
 @app.route('/login', methods = ['GET', 'POST'])
 def validateUser(): 
     pass 
@@ -16,6 +28,4 @@ def validateUser():
 
 
 if __name__ == '__main__': 
-    app.debug = True
-    app.run()
     app.run(debug = True)
