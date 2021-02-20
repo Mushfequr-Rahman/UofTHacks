bafffb2d41c0d7b0debe36c4dd0aa1300898fcb0
liangyon <yonlin.liang@mail.utoronto.ca>
2021-02-20 13:34:37 -0800

    added basic template

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask 
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def test(): 
-    return 'Test Successful'
+def index(): 
+    return render_template('index.html')
 
 if __name__ == '__main__': 
+    app.debug = True
     app.run()
+    app.run(debug = True)
