117208356738209a9d46c5f79b9256e9b529569c
Mushfequr-Rahman <rahmanmushfeq@gmail.com>
2021-02-20 17:06:47 -0500

    Added Log in Page

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 def index(): 
     return render_template('index.html')
 
+@app.route('/renderLogin')
+def renderLogin(): 
+    return render_template('login.html') 
+
+@app.route('/login', methods = ['GET', 'POST'])
+def validateUser(): 
+    pass 
+
+
+
 if __name__ == '__main__': 
     app.debug = True
     app.run()
