1e9f86e8c65b469faf436240c2c8fb0b798ee912
Mushfequr-Rahman <rahmanmushfeq@gmail.com>
2021-02-20 22:27:43 -0500

    Added a bit of Post Functionality sorry that it looks ugly

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ the project
 
 # MARK: - Libraries
 from flask import Flask, request, render_template
+import hashlib
 
 
 app = Flask(__name__)
@@ -28,10 +29,22 @@ def login():
             return render_template('home.html')
     return render_template('login.html', error=error)
 
+@app.route('/mainpage', methods=["POST"])
+def uploadFile():
+    print("Trying to upload file")
+    if request.method == 'POST':
+        print("File Upload Attempt")
+        uploadedFile = request.files['file']
+        #hashName = hashlib.sha224(uploadedFile.filename).hexdigest()
+        hashName = "Ab1ZK"
+        print("Hash Name: ", hashName)
+        if uploadedFile.filename != '': 
+            print("Send file to IPFS node") 
+            return render_template('home.html', filename=uploadedFile.filename, hash=hashName)
+   
 @app.route('/mainpage')
-def renderHomePage(): 
+def makeHome(): 
     return render_template('home.html')
-
 
 
 if __name__ == '__main__': 
